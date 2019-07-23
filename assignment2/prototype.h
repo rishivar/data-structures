48c48e45f729a808482eb91446e420f042b86471
rishivar <42106072+rishivar@users.noreply.github.com>
2019-07-23 21:49:21 +0530

    Function Prototypes

---
+++ assignment2/prototype.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "function.h"
+
+node* create();
+void insert(node *hd,int c,int o);
+void display(node *hd);
+void removedup(node *hd);
+node* multiply(node *p1,node *p2);
+node* add(node *p1,node *p2);
+
