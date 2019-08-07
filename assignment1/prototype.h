ef82b46ceeb523f0b84271dc61b1dc635f9283bb
Rishi <42106072+rishivar@users.noreply.github.com>
2019-08-07 14:49:06 +0530

    Create prototype.h

---
+++ assignment1/prototype.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "function.h"
+
+void dispall(struct student list[],int n);
+void dispnext(struct student list[],int n);
+void insertend(struct student *list,int n);
+void create(struct student *list,int n);
+void delete(struct student *list, int n);
+void insert(struct student *list,int n);
+void search(struct student list[],int n);
+void insertbeg(struct student *list,int n);
